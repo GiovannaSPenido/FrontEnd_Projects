bf3202a2286aabc18d617fc8e7388b3ff70e87fd
Giovanna Penido <127809447+GiovannaSPenido@users.noreply.github.com>
2023-12-17 15:37:20 -0300

    Delete  directory

--- TRABALHOS/TRABALHO FINAL/TRABALHO FINAL/pages/scriptFg.js
+++
@@ -1,7 +0,0 @@
-const fieldEnviado = document.getElementById('enviado');
-const btnSendRequest = document.getElementById('btn-send');
-
-btnSendRequest.addEventListener('click', () => {
-  fieldEnviado.innerHTML = 'Request successfully sent to Email'
-  fieldEnviado.style.color = '#A00CE5'
-})
